993c7da49b03ed4ea9ddf3abf5b88d9fa1c3414e
Luciene Borges <Luciene Borges>
2020-07-28 21:57:44 -0300

    rotas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Logo from '../../assets/img/Logo.png';
-import './Menu.css';
-import Button from '../Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/img/Logo.png";
+import "./Menu.css";
+import Button from "../Button";
 // import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="AluraFlix logo" />
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CadastroVideo from "./pages/cadastro/Video";
+import CadastroCategoria from "./pages/cadastro/Categoria";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
